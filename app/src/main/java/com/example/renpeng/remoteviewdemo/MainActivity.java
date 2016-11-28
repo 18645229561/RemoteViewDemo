21ef1485a51ea5db291c2c01caf1dce1f8e4477f
RenPeng <renpeng511226@gmail.com>
2016-11-28 21:16:48 +0800

    增加RemoteView

--- app/src/main/java/com/example/renpeng/remoteviewdemo/MainActivity.java
+++ app/src/main/java/com/example/renpeng/remoteviewdemo/MainActivity.java
@@ -6,12 +6,11 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.RemoteViews;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,18 +22,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mTextView = (TextView) findViewById(R.id.text);
         mTextView.setOnClickListener(new View.OnClickListener() {
-            @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+            @TargetApi(Build.VERSION_CODES.N)
             @Override
             public void onClick(View v) {
-//                Notification notification = new Notification();
-//                notification.icon = R.mipmap.ic_launcher;
-//                notification.tickerText = "HELLO WORLD";
-//                notification.flags = Notification.FLAG_AUTO_CANCEL;
+//                Notification.Builder builder = new Notification.Builder(MainActivity.this);
 //                Intent intent = new Intent(MainActivity.this,MainActivity.class);
 //                PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this,0,intent,PendingIntent.FLAG_UPDATE_CURRENT);
-//                notification
+//                builder.setContentIntent(pendingIntent);
+//                builder.setSmallIcon(R.mipmap.ic_launcher);
+//                builder.setWhen(System.currentTimeMillis());
+//                builder.setContentText("内容");
+//                builder.setContentTitle("标题");
+//                builder.setTicker("状态栏");
+//                builder.setOngoing(false);
+//                builder.setSound(Uri.withAppendedPath(MediaStore.Audio.Media.INTERNAL_CONTENT_URI,"1"));
+//                builder.setVibrate(new long[]{2000,1000,4000});
+//                Notification notification = builder.build();
 //                NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 //                manager.notify(1,notification);
+
                 Notification.Builder builder = new Notification.Builder(MainActivity.this);
                 Intent intent = new Intent(MainActivity.this,MainActivity.class);
                 PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this,0,intent,PendingIntent.FLAG_UPDATE_CURRENT);
@@ -44,12 +50,16 @@ public class MainActivity extends AppCompatActivity {
                 builder.setContentText("内容");
                 builder.setContentTitle("标题");
                 builder.setTicker("状态栏");
-                builder.setOngoing(false);
-                builder.setSound(Uri.withAppendedPath(MediaStore.Audio.Media.INTERNAL_CONTENT_URI,"1"));
-                builder.setVibrate(new long[]{2000,1000,4000});
+                RemoteViews remoteViews = new RemoteViews(getPackageName(),R.layout.remote);
+                remoteViews.setTextViewText(R.id.text1,"11111");
+                remoteViews.setImageViewResource(R.id.icon,R.drawable.iii);
+                remoteViews.setOnClickPendingIntent(R.id.text1,pendingIntent);
+                builder.setContent(remoteViews);
                 Notification notification = builder.build();
                 NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
                 manager.notify(1,notification);
+
+
             }
         });
     }
